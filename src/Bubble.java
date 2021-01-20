d804d92abefec0ea9a278073000ed724b2565160
tsukerov <tsukerovilja@gmail.com>
2021-01-20 15:33:34 +0300

    first commit

---
+++ src/Bubble.java
@@ -0,0 +1,34 @@
+class Bubble {
+    public static void main(String args[]) {
+        String nums[] = { "One", "Three", "A", "18", "Four",
+                "Hello, my friend!", "Delete", "Java!!!", "Christmas", "Happy New Year!" };
+        int a, b;
+        int size;
+        String t = new String();
+
+        size = nums.length; // number of elements to sort
+
+        // display original array
+        System.out.print("Original array is:");
+        for(int i=0; i < size; i++)
+            System.out.print(" " + nums[i]);
+        System.out.println();
+
+        // This is the bubble sort.
+        for(a=1; a < size; a++)
+            for(b=size-1; b >= a; b--) {
+                if(nums[b-1].length() > nums[b].length()) { // if out of order
+                    // exchange elements
+                    t = nums[b-1];
+                    nums[b-1] = nums[b];
+                    nums[b] = t;
+                }
+            }
+
+        // display sorted array
+        System.out.print("Sorted array is:");
+        for(int i=0; i < size; i++)
+            System.out.print(" " + nums[i]);
+        System.out.println();
+    }
+}
